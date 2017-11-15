0c44e2eb42664606975a98ff04ff1d662e3de47e
zatsepina <dadaka1996@mail.ru>
2017-11-15 20:07:02 +0700

    f

---
+++ new 1.php
@@ -0,0 +1,378 @@
+<?php
+ $data = array (
+  0 => 
+  array (
+    'id' => 6335672,
+    'name' => 'HD Webcam C270',
+    'kind' => 'веб-камера',
+    'description' => '1280x720, USB 2.0, микрофон',
+    'categoryId' => 4684840,
+    'category' => 
+    array (
+      'id' => 4684840,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'Веб-камеры',
+    ),
+    'prices' => 
+    array (
+      'amount' => '1510',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/6335672.jpeg',
+      'width' => 371,
+      'height' => 299,
+    ),
+    'link' => 'https://market.yandex.ru/product/6335672?hid=4684840&pp=1001',
+    'vendorId' => 444293,
+    'vendor' => 'Logitech',
+    'isGroup' => false,
+    'reviewsCount' => 231,
+    'rating' => 4.5,
+    'offersCount' => 54,
+    'gradeCount' => 625,
+    'articlesCount' => 0,
+    'isNew' => 0,
+  ),
+  1 => 
+  array (
+    'id' => 7878358,
+    'name' => 'HD Pro Webcam C920',
+    'kind' => 'веб-камера',
+    'description' => '1920x1080, USB 2.0, автофокус, микрофон',
+    'categoryId' => 4684840,
+    'category' => 
+    array (
+      'id' => 4684840,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'Веб-камеры',
+    ),
+    'prices' => 
+    array (
+      'amount' => '6550',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/7878358.jpeg',
+      'width' => 524,
+      'height' => 576,
+    ),
+    'link' => 'https://market.yandex.ru/product/7878358?hid=4684840&pp=1001',
+    'vendorId' => 444293,
+    'vendor' => 'Logitech',
+    'isGroup' => false,
+    'reviewsCount' => 56,
+    'rating' => 4.0,
+    'offersCount' => 30,
+    'gradeCount' => 134,
+    'articlesCount' => 4,
+    'isNew' => 0,
+  ),
+  2 => 
+  array (
+    'id' => 7073737,
+    'name' => 'LifeCam HD-3000',
+    'kind' => 'веб-камера',
+    'description' => '1280x720, USB 2.0, микрофон',
+    'categoryId' => 4684840,
+    'category' => 
+    array (
+      'id' => 4684840,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'Веб-камеры',
+    ),
+    'prices' => 
+    array (
+      'amount' => '1820',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/7073737.jpeg',
+      'width' => 463,
+      'height' => 701,
+    ),
+    'link' => 'https://market.yandex.ru/product/7073737?hid=4684840&pp=1001',
+    'vendorId' => 704572,
+    'vendor' => 'Microsoft',
+    'isGroup' => false,
+    'reviewsCount' => 32,
+    'rating' => 4.5,
+    'offersCount' => 51,
+    'gradeCount' => 72,
+    'articlesCount' => 0,
+    'isNew' => 0,
+  ),
+  3 => 
+  array (
+    'id' => 7349566,
+    'name' => 'HD Webcam C525',
+    'kind' => 'веб-камера',
+    'description' => '1280x720, USB 2.0, автофокус, микрофон',
+    'categoryId' => 4684840,
+    'category' => 
+    array (
+      'id' => 4684840,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'Веб-камеры',
+    ),
+    'prices' => 
+    array (
+      'amount' => '3254',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+      'discount' => '23',
+      'base' => '2756.25',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/7349566.jpeg',
+      'width' => 701,
+      'height' => 520,
+    ),
+    'link' => 'https://market.yandex.ru/product/7349566?hid=4684840&pp=1001',
+    'vendorId' => 444293,
+    'vendor' => 'Logitech',
+    'isGroup' => false,
+    'reviewsCount' => 37,
+    'rating' => 4.5,
+    'offersCount' => 51,
+    'gradeCount' => 93,
+    'articlesCount' => 0,
+    'isNew' => 0,
+  ),
+  4 => 
+  array (
+    'id' => 6355311,
+    'name' => 'HD Webcam C310',
+    'kind' => 'веб-камера',
+    'description' => '1280x720, USB 2.0, микрофон',
+    'categoryId' => 4684840,
+    'category' => 
+    array (
+      'id' => 4684840,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'Веб-камеры',
+    ),
+    'prices' => 
+    array (
+      'amount' => '2446',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/6355311.jpeg',
+      'width' => 701,
+      'height' => 691,
+    ),
+    'link' => 'https://market.yandex.ru/product/6355311?hid=4684840&pp=1001',
+    'vendorId' => 444293,
+    'vendor' => 'Logitech',
+    'isGroup' => false,
+    'reviewsCount' => 72,
+    'rating' => 4.5,
+    'offersCount' => 31,
+    'gradeCount' => 227,
+    'articlesCount' => 0,
+    'isNew' => 0,
+  ),
+  5 => 
+  array (
+    'id' => 7323177,
+    'name' => 'Webcam C170',
+    'kind' => 'веб-камера',
+    'description' => '0.30 МП, 640x480, USB 2.0, микрофон',
+    'categoryId' => 4684840,
+    'category' => 
+    array (
+      'id' => 4684840,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'Веб-камеры',
+    ),
+    'prices' => 
+    array (
+      'amount' => '1143',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/7323177.jpeg',
+      'width' => 661,
+      'height' => 429,
+    ),
+    'link' => 'https://market.yandex.ru/product/7323177?hid=4684840&pp=1001',
+    'vendorId' => 444293,
+    'vendor' => 'Logitech',
+    'isGroup' => false,
+    'reviewsCount' => 29,
+    'rating' => 4.5,
+    'offersCount' => 46,
+    'gradeCount' => 104,
+    'articlesCount' => 0,
+    'isNew' => 0,
+  ),
+  6 => 
+  array (
+    'id' => 6523668,
+    'name' => 'B910 HD',
+    'kind' => 'веб-камера',
+    'description' => '5 МП, 1280x720, USB 2.0, автофокус, микрофон',
+    'categoryId' => 4684840,
+    'category' => 
+    array (
+      'id' => 4684840,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'Веб-камеры',
+    ),
+    'prices' => 
+    array (
+      'amount' => '5509',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/6523668.jpeg',
+      'width' => 410,
+      'height' => 236,
+    ),
+    'link' => 'https://market.yandex.ru/product/6523668?hid=4684840&pp=1001',
+    'vendorId' => 444293,
+    'vendor' => 'Logitech',
+    'isGroup' => false,
+    'reviewsCount' => 45,
+    'rating' => 4.5,
+    'offersCount' => 35,
+    'gradeCount' => 152,
+    'articlesCount' => 0,
+    'isNew' => 0,
+  ),
+  7 => 
+  array (
+    'id' => 7697018,
+    'name' => 'HD Webcam C615',
+    'kind' => 'веб-камера',
+    'description' => '2 МП, 1920x1080, USB 2.0, автофокус, микрофон',
+    'categoryId' => 4684840,
+    'category' => 
+    array (
+      'id' => 4684840,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'Веб-камеры',
+    ),
+    'prices' => 
+    array (
+      'amount' => '4760',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/7697018.jpeg',
+      'width' => 571,
+      'height' => 543,
+    ),
+    'link' => 'https://market.yandex.ru/product/7697018?hid=4684840&pp=1001',
+    'vendorId' => 444293,
+    'vendor' => 'Logitech',
+    'isGroup' => false,
+    'reviewsCount' => 19,
+    'rating' => 4.5,
+    'offersCount' => 32,
+    'gradeCount' => 44,
+    'articlesCount' => 0,
+    'isNew' => 0,
+  ),
+  8 => 
+  array (
+    'id' => 10393061,
+    'name' => 'HD Webcam C930e',
+    'kind' => 'веб-камера',
+    'description' => '3 МП, 1920x1080, USB 2.0, автофокус, микрофон',
+    'categoryId' => 4684840,
+    'category' => 
+    array (
+      'id' => 4684840,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'Веб-камеры',
+    ),
+    'prices' => 
+    array (
+      'amount' => '8699',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/10393061.jpeg',
+      'width' => 581,
+      'height' => 701,
+    ),
+    'link' => 'https://market.yandex.ru/product/10393061?hid=4684840&pp=1001',
+    'vendorId' => 444293,
+    'vendor' => 'Logitech',
+    'isGroup' => false,
+    'reviewsCount' => 10,
+    'rating' => 4.5,
+    'offersCount' => 24,
+    'gradeCount' => 21,
+    'articlesCount' => 0,
+    'isNew' => 0,
+  ),
+  9 => 
+  array (
+    'id' => 8444244,
+    'name' => 'C-2525HD',
+    'kind' => 'веб-камера',
+    'description' => '2 МП, 1600x1200, USB 2.0, ручной фокус, микрофон',
+    'categoryId' => 4684840,
+    'category' => 
+    array (
+      'id' => 4684840,
+      'type' => 'GURU',
+      'advertisingModel' => 'HYBRID',
+      'name' => 'Веб-камеры',
+    ),
+    'prices' => 
+    array (
+      'amount' => '737',
+      'curCode' => 'RUR',
+      'curName' => 'руб.',
+    ),
+    'mainPhoto' => 
+    array (
+      'url' => 'photos/8444244.jpeg',
+      'width' => 469,
+      'height' => 701,
+    ),
+    'link' => 'https://market.yandex.ru/product/8444244?hid=4684840&pp=1001',
+    'vendorId' => 704567,
+    'vendor' => 'Defender',
+    'isGroup' => false,
+    'reviewsCount' => 2,
+    'rating' => 3.5,
+    'offersCount' => 25,
+    'gradeCount' => 13,
+    'articlesCount' => 0,
+    'isNew' => 0,
+  ),
+);
+?>
+
+
